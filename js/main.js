31093e097e43a0afd4f011ca23ebd4b61c39f609
smokingpun <hannah.adcock@gmail.com>
2014-01-22 12:05:57 +0000

    Only show jsPlumb connectors above breakpoint width

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-
+var breakpoint = 700;
+var jsPlumbConnected = false;
 
 jsPlumb.ready(function() {
-  jsPlumb.Defaults.ConnectionsDetachable = false;
-  jsPlumb.Defaults.PaintStyle = {
-    lineWidth:4,
-    strokeStyle: 'rgba(200,0,0,0.5)'
-  };
-  var common = {
-    endpoint: "Blank",
-    anchors: ["Bottom","Top"]
-  };
-  jsPlumb.connect({source:"bullet-1", target:"bullet-2"}, common);
-  jsPlumb.connect({source:"bullet-2", target:"bullet-3"}, common);
-  jsPlumb.connect({source:"bullet-3", target:"bullet-4"}, common);
-  jsPlumb.repaintEverything();
+  if(window.innerWidth > breakpoint) {
+    setupJsPlumb();
+  }
 });
 
 $(document).ready(function() {
@@ -21,20 +12,41 @@ $(document).ready(function() {
 });
 
 $(window).resize(function(){
-  jsPlumb.repaintEverything();
   onBreakpoint();
 });
 
+function setupJsPlumb() {
+  if(!jsPlumbConnected) {
+    jsPlumbConnected = true;
+    jsPlumb.Defaults.ConnectionsDetachable = false;
+    jsPlumb.Defaults.PaintStyle = {
+      lineWidth:4,
+      strokeStyle: 'rgba(200,0,0,0.5)'
+    };
+    var common = {
+      endpoint: "Blank",
+      anchors: ["Bottom","Top"]
+    };
+    jsPlumb.connect({source:"bullet-1", target:"bullet-2"}, common);
+    jsPlumb.connect({source:"bullet-2", target:"bullet-3"}, common);
+    jsPlumb.connect({source:"bullet-3", target:"bullet-4"}, common);
+    setTimeout(jsPlumb.repaintEverything, 100)
+  };
+}
+
 function onBreakpoint() {
-  var breakpoint = 700;
   var formSelector = '#convert_kit_form .ck_embed_form';
   var vertical = 'ck_vertical_subscription_form';
   var horizontal = 'ck_horizontal_subscription_form';
   if(window.innerWidth > breakpoint) {
     // Desktop layout
-    $(formSelector).removeClass(vertical).addClass(horizontal)
+    $(formSelector).removeClass(vertical).addClass(horizontal);
+    setupJsPlumb();
+    jsPlumb.repaintEverything();
   } else {
     // Mobile layout
-    $(formSelector).removeClass(horizontal).addClass(vertical)
+    $(formSelector).removeClass(horizontal).addClass(vertical);
+    jsPlumb.reset();
+    jsPlumbConnected = false;
   }
 }
